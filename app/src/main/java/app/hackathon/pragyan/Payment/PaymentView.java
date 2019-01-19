a0cf9e97b85f5e2bac72628151970b826871b04b
VivekUchiha <satsvivek@gmail.com>
2019-01-20 01:06:45 +0530

    paymentview added

---
+++ app/src/main/java/app/hackathon/pragyan/Payment/PaymentView.java
@@ -0,0 +1,15 @@
+package app.hackathon.pragyan.Payment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import app.hackathon.pragyan.R;
+
+import android.os.Bundle;
+
+public class PaymentView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_view);
+    }
+}
