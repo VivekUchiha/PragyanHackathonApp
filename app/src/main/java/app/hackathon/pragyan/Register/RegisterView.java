8b16aec66fc75370e92190b24f378d51e8071637
VivekUchiha <satsvivek@gmail.com>
2019-01-19 21:26:27 +0530

    add register layout

--- app/src/main/java/app/hackathon/pragyan/Register/RegisterView.java
+++ app/src/main/java/app/hackathon/pragyan/Register/RegisterView.java
@@ -4,12 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import app.hackathon.pragyan.R;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterView extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_view);
+        EditText username = findViewById(R.id.username);
+        EditText email = findViewById(R.id.email);
+        EditText password = findViewById(R.id.password);
+        EditText uid = findViewById(R.id.uid);
+        EditText shopid = findViewById(R.id.shopid);
+
+        Button register = findViewById(R.id.registerbutton);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo register in server
+                //todo check if uid unique and toast if not
+
+            }
+        });
+
     }
 }
