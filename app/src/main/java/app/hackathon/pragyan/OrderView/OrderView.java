0900c10b28c43f7f9df0274601c1e517aa62ecc8
VivekUchiha <satsvivek@gmail.com>
2019-01-19 18:46:28 +0530

    created activities and basic model

---
+++ app/src/main/java/app/hackathon/pragyan/OrderView/OrderView.java
@@ -0,0 +1,16 @@
+package app.hackathon.pragyan.OrderView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import app.hackathon.pragyan.R;
+
+import android.os.Bundle;
+
+public class OrderView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+    }
+}
